d25f7ad4ab778dedd82f37e7a38527ada2910678
Guilherme Pedroso <guilhermepedrosogg@ggmail.com>
2023-01-27 18:56:46 -0300

    mult-array

---
+++ prog14.cpp
@@ -0,0 +1,21 @@
+/*
+  Title: Mult-Arrays
+  Author: Guilherme Pedroso
+  Date: Jan 2023
+  Updated: --
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int
+main()
+  {
+  int mult_array[3][3] = {{11,12,13},
+	                  {21,22,23},
+			  {31,32,33}};
+  cout << "Array 3x3 position 22 =  " << mult_array[1][1] << endl;
+  return(0);
+  }
+
