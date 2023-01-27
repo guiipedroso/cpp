1be4f7c35bf28154094361ae046fda6231d4c099
Guilherme Pedroso <guilhermepedrosogg@ggmail.com>
2023-01-27 18:14:11 -0300

    aritmetics

---
+++ prog5.cpp
@@ -0,0 +1,24 @@
+/*
+  Title: Aritmetics
+  Author: Guilherme Pedroso
+  Date: Jan 2023
+  Updated: --
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int
+main()
+  {
+  int x = 15, Y = 9;
+
+  cout << "X + Y = " << x + Y << endl;
+  cout << "X - Y = " << x - Y << endl;
+  cout << "X * Y = " << x * Y << endl;
+  cout << "X : Y = " << x / Y << endl;
+  cout << "X %  Y = " << x % Y << endl;
+  
+  return(0);
+  }
