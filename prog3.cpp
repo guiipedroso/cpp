37bc6715eb5fd27be6694ac7019a71f93aac360c
Guilherme Pedroso <guilhermepedrosogg@ggmail.com>
2023-01-26 21:50:58 -0300

    comments

---
+++ prog3.cpp
@@ -0,0 +1,24 @@
+/*
+  Title: Comments
+  Author: Guilherme Pedroso
+  Date: Jan 2023
+  Updated: --
+*/
+
+
+#include <iostream> // header or cabeçalho
+// Comentario
+using namespace std; // serve para usar as instruções cout e endl
+
+int
+main() //função fundamental
+
+/*
+ 
+Hi my friend, welcome to my github
+
+*/
+
+
+  return(0);
+  }
