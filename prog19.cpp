5ccaab9c3e6fe1865f82af4646e746b2f0526cb6
Guilherme Pedroso <guilhermepedrosogg@gmail.com>
2023-03-28 14:15:39 -0300

    Expression ?

---
+++ prog19.cpp
@@ -0,0 +1,20 @@
+/*
+  Title: Expression ?
+  Author: Guilherme Pedroso
+  Date: Mar 2023
+  Updated: --
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int 
+main()
+  {
+  short int num1 = 9;
+
+  std::cout << "eh maior que num? "<< (num1 >= 8? "Sim, maior!" : "Nao, menor!") << std::endl;
+
+  return(0);
+  }
