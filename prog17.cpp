cff7308b5c3d477ef04ec9f21a93b419875c4fca
Guilherme Pedroso <guilhermepedrosogg@gmail.com>
2023-03-28 13:17:34 -0300

    Input

---
+++ prog17.cpp
@@ -0,0 +1,19 @@
+/*
+  Title: Input - Word
+  Author: Guilherme Pedroso
+  Date: Mar 2023
+  Updated: --
+*/
+
+#include <iostream>
+
+int 
+main()
+  {
+  char letra {};
+  std::cout << "Informe um letra: ";
+  std::cin >> letra;
+  std::cout << "A letra que voce digitou foi: " << letra << "\n";
+
+  return(0);
+  }
