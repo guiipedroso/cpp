e0b9c0d317bbfac6dcc6559e4a24ae5956174e80
Guilherme Pedroso <guilhermepedrosogg@gmail.com>
2023-05-01 11:14:35 -0300

    Recursive

---
+++ prog30.c
@@ -0,0 +1,22 @@
+/*
+  Title: Recursive
+  Author: Guilherme Pedroso
+  Date: May 2023
+  Updated: --
+*/
+
+#include <iostream> //input or output 
+
+using namespace std;
+
+void funcao()
+  {
+  std::cout << "Eu sou alguém" << std::endl;
+  }
+
+int
+main()
+  {
+  funcao();
+  return(0);
+  }
